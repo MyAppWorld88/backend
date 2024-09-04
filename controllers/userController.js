6530afe5c6f4c7f1ab1fa62233171008e365d3b8
upanshu <upanshusharm89@gmail.com>
2024-09-04 17:13:33 +0530

    changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,35 @@
 const asyncHander = require("express-async-handler");
+const bcrypt= require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 //@desc Register a User
 //@route Post /api/v1/users/register
 //@access public
 const registerUser = asyncHander(async(req,res)=>{
-  res.json({message:"Register the User"});
+    const {username,email,password}=req.body;
+    if(!username || !email || !password){
+       res.status(400);
+       throw new Error("All fields are mandatory!");
+    }
+    const userAvailable = await User.findOne({email});
+    if(userAvailable){
+      res.status(400);
+      throw new Error("User Already Registered"); 
+    }
+    //hash password
+    const hashPassword = await bcrypt.hash(password,10);
+
+    const user = await User.create({
+        username,email,password:hashPassword,
+    })
+    if(user){
+        res.status(201).json({_id:user.id,email:user.email});
+    }else{
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
+
+//   res.json({message:"Register the User"});
 });
 
 //@desc login a User
